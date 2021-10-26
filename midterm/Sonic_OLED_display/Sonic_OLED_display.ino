614f5082ac5a0d8b77f27770d2b45a1998a30cd2
unknown <edwardj.i@hotmail.com>
2021-10-26 15:04:57 -0600

    midday

--- midterm/Sonic_OLED_display/Sonic_OLED_display.ino
+++ midterm/Sonic_OLED_display/Sonic_OLED_display.ino
@@ -1,4 +1,4 @@
-
+#include "oledHue.h"
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -6,7 +6,7 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define SCREEN_ADDRESS 0x3c
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
 
 
 const int trigpin = 6;
@@ -39,21 +39,38 @@ void loop() {
   duration = pulseIn(echopin, HIGH);
   distance = duration*0.034/2/2.54; // converts cm to inches
   Serial.printf("Distance %0.1f inches \n",distance);
-  if(distance <= 10){ // when within 10 in, turns on led 
+  if(distance <= 3){ // when within 10 in, turns on led 
     welcomeInfo(); 
   }
   else {
-    display.clearDisplay(); // returns blank screen after leaving 10 inches away from sensor
-    display.display();
   }
-  
+  if(distance>10){
+   hueinfo();
+  }
+  else {
+    //display.clearDisplay(); // returns blank screen after leaving 10 inches away from sensor
+    //display.display();
+    
+  }
 
 }
-void welcomeInfo(){ // sample display to be replaced with bme readings
+void welcomeInfo(){
+  int rot = 2;// sample display to be temp,humidity,time
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0,0);
   display.printf("Welcome info");
   display.display();
+  display.setRotation(rot);
+}
+void hueinfo(){
+  int rot = 2;// sample display to be replaced which hue lights are on
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.printf("hue info");
+  display.display();
+  display.setRotation(rot);
 }
---
+++ midterm/Sonic_OLED_display/oledHue.h
@@ -0,0 +1,40 @@
+
+#include <OneButton.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SCREEN_ADDRESS 0x3c
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+bool buttonState;
+void click1(){
+  buttonState = !buttonState;
+}
+
+
+
+
+void oledHue(){
+  OneButton hueButtPin(20, false);
+  buttonState = false;
+  hueButtPin.tick();
+  hueButtPin.attachClick(click1);
+  if(buttonState){
+  int rot = 2;// sample display to be replaced with bme readings
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.printf("Hue lights on");
+  display.display();
+  display.setRotation(rot);
+
+  }
+  
+  else {
+ //display.clearDisplay();
+ //display.display();
+  }
+  }
